11ff7a9206fc5c1e53895f5ffcda9974b2ddeb31
rajesh <rajesh.t7982@gmail.com>
2024-06-17 20:48:00 +0530

    session-17

---
+++ session-js-17/project-1/script.js
@@ -0,0 +1,41 @@
+//JS is dynamically typed language
+//statically typed lang
+
+let a;
+
+
+console.log('value of a is ',a)
+console.log('datatype of a is ',typeof a)
+
+a=10;
+console.log('value of a is ',a)
+console.log('datatype of a is ',typeof a)
+
+
+a='hello';
+console.log('value of a is ',a)
+console.log('datatype of a is ',typeof a)
+
+a=true;
+console.log('value of a is ',a)
+console.log('datatype of a is ',typeof a)
+
+
+
+//array
+let skills=['css','js','react'];
+let marks=[90,89,50];
+
+
+//object
+let person={
+    pid:100,
+    name:'ravi',
+    age:25,
+    city:'hyd'
+}
+
+//function
+function test(a,b){
+    //body of the function
+}
---
+++ session-js-17/project-2/script.js
@@ -0,0 +1,44 @@
+//aritnmetic operators
+//+ - * / %
+
+let a=100;
+let b=20;
+
+console.log("sum is ",a+b);
+console.log("diff is ",a-b);
+console.log("product is ",a*b);
+console.log("quotient is ",a/b);
+console.log("reminder is ",a%b);
+
+
+//Assignment operator
+let x=200;
+
+//equal to operator
+//   == equal to
+//   === strict equal to
+let m=200;
+let n=100;
+console.log(m==n)
+
+let p=121;
+let q='121';
+
+console.log(typeof p)
+console.log(typeof q)
+
+console.log(p==q)
+console.log(p===q)
+
+
+
+//comparision operators
+// == === > >= < <= != !==
+let e=123;
+let f=234;
+console.log(e>f)
+
+
+//logical operators
+// && || !
+console.log(e>100 || f<200)
